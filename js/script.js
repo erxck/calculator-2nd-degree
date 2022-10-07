618af85274ad03f894781b6647e80ac23c5ee2cd
erxck <erickrian2002@gmail.com>
2022-10-07 17:00:11 -0300

    upgrade

--- js/script.js
+++ js/script.js
@@ -1,33 +1,41 @@
 function Eq2Grau() {
-    const a = document.querySelector('#a').value;
-    const b = document.querySelector('#b').value;
-    const c = document.querySelector('#c').value;
+    const a = Number(document.querySelector('#a').value);
+    const b = Number(document.querySelector('#b').value);
+    const c = Number(document.querySelector('#c').value);
     const reply = document.querySelector('#reply');
     let delta = 0;
     let valorDeX = 0;
     let valorDeX1 = 0;
     let valorDeX2 = 0;
 
-    if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c)) {
-        reply.innerHTML = "Apenas números são validos!";
-    } else {
-        delta = Math.pow(b, 2) - 4 * a * c;
+    console.log(typeof a, typeof b, typeof c)
 
+    if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c)) {
+        delta = (Math.pow(b, 2) - 4) * a * c;
+        
         if (delta == 0) {
             valorDeX = (-(b)) / (2 * a);
-
-            reply.innerHTML = `X: ${valorDeX}`;
-        } else if (delta < 0) {
-            reply.innerHTML = `${delta} delta negativo, logo não temos raízes reais.`;
-        } else {
-            valorDeX1 = (-b + Math.sqrt(delta)) / (2 * a);
-            valorDeX2 = (-b - Math.sqrt(delta)) / (2 * a);
-
+            
             reply.innerHTML = `
-                As raízes da equação ${a == 1 ? '' : a}x² ${b == 0 ? '' : b}${b != 0 ? 'x' : ''} ${c} = 0 são:
+                A raíz da equação ${a == 1 ? '' : a}x² ${b > 0 ? '+' : ''} ${b == 0 ? '' : b}${b != 0 ? 'x' : ''} ${c > 0 ? '+' : ''} ${c == 0 ? '' : c} = 0 é:
                 <br>
-                x¹ = ${valorDeX1} e x² = ${valorDeX2}
+                x = ${valorDeX}
             `;
+        } else if (delta > 0) {
+            valorDeX1 = (-(b) + Math.sqrt(delta)) / (2 * a);
+            valorDeX2 = (-(b) - Math.sqrt(delta)) / (2 * a);
+            
+            reply.innerHTML = `
+                As raízes da equação ${a == 1 ? '' : a}x² ${b > 0 ? '+' : ''} ${b == 0 ? '' : b}${b != 0 ? 'x' : ''} ${c > 0 ? '+' : ''} ${c == 0 ? '' : c} = 0 são:
+                <br>
+                x¹ = ${valorDeX1}
+                <br>
+                x² = ${valorDeX2}
+            `;
+        } else {
+            reply.innerHTML = `${delta} delta negativo, logo não temos raízes reais.`;
         }
+    } else {
+        reply.innerHTML = "Apenas números são válidos!";
     }
 }

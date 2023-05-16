7c7b9879af69e10d5c8bee26d5f1ba4b6f64f3b0
erxck <erickrian2002@gmail.com>
2023-05-16 08:03:16 -0300

    bugs fixed

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Calculator for 2nd degree equations
-# Calculadora para equações do 2º grau
+# Calculadora para equação do 2º grau
+
+> Esta é uma calculadora para equação do 2º grau, feita com HTML, CSS e JavaScript.
+
+## Tecnologias
+
+- [HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [JavaScript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+
+## Introdução
+
+Este foi uns dos meus primeiros projetos quando estava aprendendo Javascript. O projeto consiste em uma calculadora para equação do 2º grau, onde o usuário insere os valores de A, B e C e a calculadora retorna o valor de X1 e X2.
+
+## Como instalar
+
+- Para baixar o projeto, siga as instruções abaixo.
+
+```
+1. git clone https://github.com/erxck/calculator-2nd-degree
+2. cd calculator-2nd-degree
+```
+
+## Manter contato
+
+Erick Nunes
+
+- [contatoerickrian@gmail.com](mailto:contatoerickrian@gmail.com)
+- [github.com/erxck](https://github.com/erxck)
+- [linkedin.com/in/erick-rian](https://linkedin.com/in/erick-rian)
+
+E é isso! 😃💻
--- js/script.js
+++ js/script.js
@@ -1,39 +1,67 @@
 function Eq2Grau() {
-    const a = Number(document.querySelector('#a').value);
-    const b = Number(document.querySelector('#b').value);
-    const c = Number(document.querySelector('#c').value);
-    const reply = document.querySelector('#reply');
-    let delta = 0;
-    let valorDeX = 0;
-    let valorDeX1 = 0;
-    let valorDeX2 = 0;
+  const a = parseInt(document.querySelector("#a").value) || 0;
+  const b = parseInt(document.querySelector("#b").value) || 0;
+  const c = parseInt(document.querySelector("#c").value) || 0;
+  const reply = document.querySelector("#reply");
+  let delta = 0;
+  let valorDeX = 0;
+  let valorDeX1 = 0;
 
-    if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c)) {
-        delta = (Math.pow(b, 2) - 4) * a * c;
-        
-        if (delta == 0) {
-            valorDeX = (-(b)) / (2 * a);
-            
-            reply.innerHTML = `
-                A raíz da equação ${a == 1 ? '' : a}x² ${b > 0 ? '+' : ''} ${b == 0 ? '' : b}${b != 0 ? 'x' : ''} ${c > 0 ? '+' : ''} ${c == 0 ? '' : c} = 0 é:
+  if (a === 0) {
+    reply.innerHTML = "A equação não é de segundo grau!";
+  } else if (b === 0) {
+    valorDeX = Math.sqrt(-c / a);
+    valorDeX1 = -Math.sqrt(-c / a);
+
+    reply.innerHTML = `
+            As raízes da equação ${a === 1 ? "" : a}x² ${b > 0 ? "+" : ""} ${
+      b === 0 ? "" : b
+    }${b != 0 ? "x" : ""} ${c > 0 ? "+" : ""} ${c === 0 ? "" : c} = 0 são:
+
+            <br>
+            x¹ = ${valorDeX}
+            <br>
+            x² = ${valorDeX1}
+        `;
+  } else if (c === 0) {
+    valorDeX = -b / a;
+
+    reply.innerHTML = `
+            As raízes da equação ${a === 1 ? "" : a}x² ${b > 0 ? "+" : ""} ${
+      b === 0 ? "" : b
+    }${b != 0 ? "x" : ""} ${c > 0 ? "+" : ""} ${c === 0 ? "" : c} = 0 são:
+
+            <br>
+            x = ${valorDeX}
+        `;
+  } else {
+    delta = b * b - 4 * a * c;
+
+    if (delta === 0) {
+      valorDeX = -b / (2 * a);
+
+      reply.innerHTML = `
+                A raíz da equação ${a === 1 ? "" : a}x² ${b > 0 ? "+" : ""}
+                ${b === 0 ? "" : b}${b != 0 ? "x" : ""} 
+                ${c > 0 ? "+" : ""} ${c === 0 ? "" : c} = 0 é:
                 <br>
                 x = ${valorDeX}
             `;
-        } else if (delta > 0) {
-            valorDeX1 = (-(b) + Math.sqrt(delta)) / (2 * a);
-            valorDeX2 = (-(b) - Math.sqrt(delta)) / (2 * a);
-            
-            reply.innerHTML = `
-                As raízes da equação ${a == 1 ? '' : a}x² ${b > 0 ? '+' : ''} ${b == 0 ? '' : b}${b != 0 ? 'x' : ''} ${c > 0 ? '+' : ''} ${c == 0 ? '' : c} = 0 são:
+    } else if (delta > 0) {
+      valorDeX = (-b + Math.sqrt(delta)) / (2 * a);
+      valorDeX1 = (-b - Math.sqrt(delta)) / (2 * a);
+
+      reply.innerHTML = `
+                A raíz da equação ${a === 1 ? "" : a}x² ${b > 0 ? "+" : ""}
+                ${b === 0 ? "" : b}${b != 0 ? "x" : ""} 
+                ${c > 0 ? "+" : ""} ${c === 0 ? "" : c} = 0 é:
                 <br>
-                x¹ = ${valorDeX1}
+                x = ${valorDeX}
                 <br>
-                x² = ${valorDeX2}
+                x² = ${valorDeX1}
             `;
-        } else {
-            reply.innerHTML = `${delta} delta negativo, logo não temos raízes reais.`;
-        }
     } else {
-        reply.innerHTML = "Apenas números são válidos!";
+      reply.innerHTML = `${delta} delta negativo, logo não temos raízes reais.`;
     }
+  }
 }
